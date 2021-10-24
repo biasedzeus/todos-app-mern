6f4e225139dcbb49a99afafb28187e44d6dbe509
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-24 06:56:28 +0530

    materializeCSS & form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import Preloader from './Components/Preloader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div classname="center-align">
+  <div className="row">
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s4">
+          <i className="material-icons prefix">title</i>
+          <input id="icon_prefix" type="text" className="validate" />
+          <label htmlFor="icon_prefix">title</label>
+        </div>
+        <div className="input-field col s5">
+          <i className="material-icons prefix">description</i>
+          <input id="description" type="tel" className="validate" />
+          <label htmlFor="description">content</label>
+        </div>
+        <div class="input-field col s1">
+        <i className="material-icons prefix">delete</i>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
   );
 }
 
