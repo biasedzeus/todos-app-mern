f07c221421ab865663fb58aadcf836b5b32e522e
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-25 21:23:32 +0530

    update todo api added

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,5 +4,6 @@ const url ="http://localhost:3300/todos";
 
 export const readTodos = () => axios.get(url);
 export const createTodo = newTodo => axios.post(url,newTodo);
+export const updateTodo = (id,updatedTodo) => axios.patch(`${url}/${id}`,updatedTodo)
 
 
