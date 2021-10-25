fa1fd8640b1d19369ee8f71fa08a833066baf91a
biasedzeus <bhanu.singh1078@gmail.com>
2021-10-25 21:45:17 +0530

    id bug fixed
    used id inplace of _id

--- backend/controller/todos.controller.js
+++ backend/controller/todos.controller.js
@@ -24,12 +24,16 @@ const Todo = require('../models/todos.model')
 const updateTodo = async (req,res) =>{
     const {id} = req.params;
     const {title,content} = req.body;
-    if(!mongoose.isValidObjectId(id)){
-        return res.status(404).send(`The Id ${id} is not valid`);
+    // if(!mongoose.isValidObjectId(id)){
+    //     return res.status(404).send(`The Id ${id} is not valid`);
 
+    // }
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).send(`The Id ${id} is not valid`);
     }
 
-    const todo = {title,content,_id};
+    const todo = {title,content,id};
     await Todo.findByIdAndUpdate(id,todo,{new:true})
     res.json(todo);
 
